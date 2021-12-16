be6eac1d2d5a7d5ea69de07a784d9e4a51b1660c
ivokund <ivo@pactum.com>
2021-12-16 04:59:39 +0200

    Day 15 part 2 naive

--- day15/index.js
+++ day15/index.js
@@ -16,6 +16,11 @@ const testInput = `1163751742
 function part1(input) {
     const rows = input.split('\n').map((r) => r.split('').map((n) => +n))
 
+    return calculate(rows)
+}
+
+
+function calculate(rows) {
     const rowMaxIndex = rows.length-1
     const colMaxIndex = rows[0].length - 1
     const finishIndex = `${rowMaxIndex},${colMaxIndex}`
@@ -39,12 +44,17 @@ function part1(input) {
 
     // add initial neighbours to the stack
     getPossiblePaths(current).forEach((toCoords) => {
-        const path = [current, toCoords,]
-        stack.push({path, score: getRisk(toCoords)})
+        const path = [current, toCoords]
+        // const visited = path.reduce((acc, [row, col]) => ({...acc, [`${row},${col}`]: true}))
+        stack.push({
+            path,
+            score: getRisk(toCoords),
+            // visited
+        })
     })
 
-
     const lowestScoresToCoords = {}
+    let winPath
     let i=0
     while (stack.length > 0) {
         i++
@@ -52,28 +62,77 @@ function part1(input) {
             console.log({i, len: stack.length, lowest: lowestScoresToCoords[finishIndex]})
         }
         const { path, score } = stack.pop()
-        for (let toCoords of getPossiblePaths(path[path.length - 1])) {
+        const lastNodeCoords = path[path.length - 1]
+        const prevToLastCoords = path[path.length - 2]
+        for (let toCoords of getPossiblePaths(lastNodeCoords)) {
             const coordIndex = toCoords.join()
+
+            // if (visited[coordIndex]) {
+            //     continue
+            // }
             const newScore = getRisk(toCoords) + score
+
+            // don't go to where we just came from
+            if (`${prevToLastCoords[0]},${prevToLastCoords[1]}` === coordIndex) {
+                continue
+            }
+            if (coordIndex === finishIndex) {
+                winPath = [...path, toCoords]
+            }
+
+            const minStepsToEnd = (colMaxIndex - toCoords[0]) + (rowMaxIndex - toCoords[1])
+            const minScoreToFinish = newScore + minStepsToEnd
+            // check if we already have a better final score than this score
+            if (minScoreToFinish > lowestScoresToCoords[finishIndex]) {
+                continue
+            }
+
             if (lowestScoresToCoords[coordIndex] === undefined || newScore < lowestScoresToCoords[coordIndex]) {
-                // console.log(`Found better score for ${coordIndex}: ${newScore} (before ${lowestScoresToCoords[coordIndex]})`)
                 lowestScoresToCoords[coordIndex] = newScore
-                stack.push({
+                // visited[`${toCoords[0]},${toCoords[1]}`] = true
+                stack.unshift({
                     path: [...path, toCoords],
-                    score: newScore
+                    score: newScore,
+                    // visited: visited
                 })
             }
         }
     }
 
     return lowestScoresToCoords[finishIndex]
-
 }
 
 
 function part2(input) {
-    const rows = input.split('\n')
+    const rowsOriginal = input.split('\n').map((r) => r.split('').map((n) => +n))
 
+    const origLen = rowsOriginal[0].length
+
+    const rowsV2 = rowsOriginal.map((row) => {
+        const newRow = []
+        let newNumbers = row
+        for (let tileX = 0; tileX < 5; tileX++) {
+            newRow.push(...newNumbers)
+            newNumbers = newNumbers.map((i) => i + 1 > 9 ? 1 : i + 1)
+        }
+        return newRow
+    })
+
+    const rowsV3 = []
+    for (let i=0; i<origLen * 5; i++) {
+        let col = rowsV2.map((row) => row[i])
+
+        for (let tileY = 0; tileY < 5; tileY++) {
+            col.forEach((number, rowIndex) => {
+                const newRowIndex = rowIndex + (tileY * origLen)
+                if (!rowsV3[newRowIndex]) { rowsV3[newRowIndex] = []}
+                rowsV3[newRowIndex][i] = number
+            })
+            col = col.map((i) => i + 1 > 9 ? 1 : i + 1)
+        }
+    }
+
+    return calculate(rowsV3)
 }
 
 console.log('-- test input')
@@ -81,5 +140,5 @@ console.log('-- test input')
 // console.log({ part2: part2(testInput) })
 
 console.log('-- real input')
-console.log({ part1: part1(realInput) })
-// console.log({ part2: part2(realInput) })
+// console.log({ part1: part1(realInput) })
+console.log({ part2: part2(realInput) })
